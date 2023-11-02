267ebc441d4c42026326988abe19ae8a75ad2ddb
Willian Ferreira <willian1948@hotmail.com>
2023-11-01 22:55:13 -0300

    sei la oq aconteceu aqui

--- public/build/assets/InputLabel-ac4d26cc.js
+++
@@ -1,1 +0,0 @@
-import{o as t,d as e,t as o,m as n}from"./app-a6b8c3ba.js";const l={class:"block font-medium text-sm text-gray-700 dark:text-gray-300"},r={key:0},c={key:1},m={__name:"InputLabel",props:{value:String},setup(a){return(s,_)=>(t(),e("label",l,[a.value?(t(),e("span",r,o(a.value),1)):(t(),e("span",c,[n(s.$slots,"default")]))]))}};export{m as _};
